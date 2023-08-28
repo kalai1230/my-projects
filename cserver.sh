16176123e50bbd66fa050b616aa484acfba0e196
kalai1230 <kalais1620@gmail.com>
2023-08-28 15:13:23 +0000

    monitoring cpu usage

---
+++ cserver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+threshold=20
+
+# Slack webhook URL
+webhook_url="https://hooks.slack.com/services/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"
+
+    # Get CPU usage percentage
+    cpu_usage=$(top -bn 1 | awk '/%Cpu/{print $2}')
+    
+    # Print CPU usage
+    echo "Current CPU Usage: $cpu_usage%"
+    # checking the threshold exceeds using if
+	 if (( $(echo "$cpu_usage > $threshold" | bc -l) )); then
+        message="CPU usage exceeded threshold. Current usage: $cpu_usage%"
+        payload="{\"text\": \"$message\"}"
+
+	#Send slack alert 
+	curl -X POST -H "Content-Type: application/json" -d "$payload" "$webhook_url"
+	 fi
+
+
